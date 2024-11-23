0ecf8881751c569ae4307a9f9e0850a4d070f597
Michele Ramos <michele.ramoszav@gmail.com>
2024-11-23 11:52:54 -0500

    fix git divergence

---
+++ src/main/java/com/example/branch_hiring_exercise/BranchHiringExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.branch_hiring_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchHiringExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchHiringExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/branch_hiring_exercise/BranchHiringExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.branch_hiring_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchHiringExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
