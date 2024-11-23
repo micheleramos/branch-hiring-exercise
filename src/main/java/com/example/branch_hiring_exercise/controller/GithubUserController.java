aca1291cb8d709ad9665ce7c3eea9fcb1989a4be
Michele Ramos <michele.ramoszav@gmail.com>
2024-11-23 12:58:41 -0500

    controller & service initial code

--- src/main/java/com/example/branch_hiring_exercise/controller/GithubUserController.java
+++ src/main/java/com/example/branch_hiring_exercise/controller/GithubUserController.java
@@ -1,5 +1,25 @@
 package com.example.branch_hiring_exercise.controller;
 
-public class GithubUserController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.branch_hiring_exercise.service.GithubUserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class GithubUserController {
+	
+	private final GithubUserService githubUserService = new GithubUserService();
+
+	@GetMapping("/{username}")
+    public String getUserData(@PathVariable String username) {
+        return githubUserService.getUserData(username);
+    }
 }
+
+
--- src/main/java/com/example/branch_hiring_exercise/service/GithubUserService.java
+++ src/main/java/com/example/branch_hiring_exercise/service/GithubUserService.java
@@ -1,5 +1,13 @@
 package com.example.branch_hiring_exercise.service;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class GithubUserService {
+
+	public String getUserData(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
