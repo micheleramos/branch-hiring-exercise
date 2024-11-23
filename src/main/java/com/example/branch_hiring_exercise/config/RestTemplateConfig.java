4d1007238050071a6429237dc898b627da5060e7
Michele Ramos <michele.ramoszav@gmail.com>
2024-11-23 12:39:02 -0500

    fix misconfigurations, run successful build after removing jpa dependency

--- src/main/java/com/example/branch_hiring_exercise/config/RestTemplateConfig.java
+++ src/main/java/com/example/branch_hiring_exercise/config/RestTemplateConfig.java
@@ -1,5 +1,14 @@
 package com.example.branch_hiring_exercise.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RestTemplateConfig {
+    @Bean
+    RestTemplateConfig restTemplate() {
+	    return new RestTemplateConfig();
+	}
 
 }
+
--- src/test/java/com/example/branch_hiring_exercise/config/RestTemplateConfigTest.java
+++ src/test/java/com/example/branch_hiring_exercise/config/RestTemplateConfigTest.java
@@ -1,11 +1,22 @@
 package com.example.branch_hiring_exercise.config;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {RestTemplateConfig.class})
 public class RestTemplateConfigTest {
-	@Test
-	void contextLoads() {
-	}
+
+    @Autowired
+    private RestTemplateConfig restTemplateConfig;
+
+    @Test
+    public void contextLoads() {
+        assertNotNull(restTemplateConfig);
+    }
 }
